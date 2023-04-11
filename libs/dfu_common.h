5c3c848d7ee1e5352a06d555067363a58e65fd49
pingley17 <130401773+pingley17@users.noreply.github.com>
2023-04-10 20:37:30 -0400

    new files

---
+++ libs/dfu_common.h
@@ -0,0 +1,16 @@
+#ifndef __DFU_COMMON_H__
+#define __DFU_COMMON_H__
+
+#define DFU_DESC_LEN 64
+#define DFU_FILE_LEN 64
+#define DFU_SHA1_LEN 20
+#define DFU_URL_LEN  256
+
+struct dfu_file_t {
+	char desc[DFU_DESC_LEN];
+	char lfile[DFU_FILE_LEN];
+	char rfile[DFU_FILE_LEN];
+	char sha1[DFU_SHA1_LEN];
+};
+
+#endif
